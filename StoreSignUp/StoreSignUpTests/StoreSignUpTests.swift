ef44a7d96d0db6e8b5bb9ae3939178b8d0171ee6
Elena <iosdevlogin@gmail.com>
2025-08-20 19:03:44 +0500

    Create project

---
+++ StoreSignUp/StoreSignUpTests/StoreSignUpTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import StoreSignUp
+
+final class StoreSignUpTests: XCTestCase {
+
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func testExample() throws {}
+
+    func testPerformanceExample() throws {
+        self.measure {}
+    }
+}
